f935e9272fe7c06a7608c94a99d2c1d0422f6624
Juan Francisco Rodriguez <zenilt@gmail.com>
2014-03-01 22:59:06 -0300

    more params for automatic forms

--- lib/bicubic/DataObject.php
+++ lib/bicubic/DataObject.php
@@ -16,22 +16,32 @@ abstract class DataObject {
      */
     public abstract function __getProperties();
 
-    /**
-     * Verifica que Contenga todos las propiedades necesarias para ingresar a BD
-     * @return true si contiene todas las propiedades necesarias, false si no
-     */
-    public abstract function __isComplete();
+    public function __isComplete() {
+        $class = strtolower(get_class($this));
+        $properties = $this->__getProperties();
+        foreach ($properties as $property) {
+            if ($property["required"]) {
+                $pname = $property["name"];
+                $getter = "get$pname";
+                $value = $this->$getter();
+                if($value === null) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
-    public abstract function __isChild();
-    
+    public function __isChild() {
+        return false;
+    }
 
     public function __getParentProperties() {
-        if($this->__isChild()) {
+        if ($this->__isChild()) {
             return $this->__getParentObject()->__getProperties();
         }
         return null;
     }
-    
 
     public function __getParentObject() {
         if ($this->__isChild()) {
@@ -54,24 +64,17 @@ abstract class DataObject {
         $properties = $this->__getProperties();
         foreach ($properties as $property) {
             $key = $property["name"];
-            $obj = strpos($key, "_object");
-            $arr = strpos($key, "_array");
-            if (!$arr) {
-                if (!$obj) {
-                    $cammel = strtoupper(substr($key, 0, 1)) . substr($key, 1);
-                    $setter = "set$cammel";
-                    $dbprop1 = $key;
-                    $dbprop2 = $class . $key;
-                    $dbprop3 = $key . "id";
-
-                    if (array_key_exists($dbprop1, $row)) {
-                        $this->$setter($row[$dbprop1]);
-                    } else if (array_key_exists($dbprop2, $row)) {
-                        $this->$setter($row[$dbprop2]);
-                    } else if (array_key_exists($dbprop3, $row)) {
-                        $this->$setter($row[$dbprop3]);
-                    }
-                }
+            $cammel = strtoupper(substr($key, 0, 1)) . substr($key, 1);
+            $setter = "set$cammel";
+            $dbprop1 = $key;
+            $dbprop2 = $class . $key;
+            $dbprop3 = $key . "id";
+            if (array_key_exists($dbprop1, $row)) {
+                $this->$setter($row[$dbprop1]);
+            } else if (array_key_exists($dbprop2, $row)) {
+                $this->$setter($row[$dbprop2]);
+            } else if (array_key_exists($dbprop3, $row)) {
+                $this->$setter($row[$dbprop3]);
             }
         }
     }
@@ -81,28 +84,20 @@ abstract class DataObject {
         $properties = $this->__getProperties();
         foreach ($properties as $property) {
             $key = $property["name"];
-            $obj = strpos($key, "_object");
-            $arr = strpos($key, "_array");
-            if (!$arr) {
-                if (!$obj) {
-                    $cammel = strtoupper(substr($key, 0, 1)) . substr($key, 1);
-                    $setter = "set$cammel";
-                    $dbprop1 = $key;
-                    $dbprop2 = $class . $key;
-                    $dbprop3 = $key . "id";
-                    if (property_exists($row, $dbprop1)) {
-                        $this->$setter($row->$dbprop1);
-                    } else if (property_exists($row, $dbprop2)) {
-                        $this->$setter($row->$dbprop2);
-                    } else if (property_exists($row, $dbprop3)) {
-                        $this->$setter($row->$dbprop3);
-                    }
-                }
+            $cammel = strtoupper(substr($key, 0, 1)) . substr($key, 1);
+            $setter = "set$cammel";
+            $dbprop1 = $key;
+            $dbprop2 = $class . $key;
+            $dbprop3 = $key . "id";
+            if (property_exists($row, $dbprop1)) {
+                $this->$setter($row->$dbprop1);
+            } else if (property_exists($row, $dbprop2)) {
+                $this->$setter($row->$dbprop2);
+            } else if (property_exists($row, $dbprop3)) {
+                $this->$setter($row->$dbprop3);
             }
         }
     }
-    
-    
 
 }
 
