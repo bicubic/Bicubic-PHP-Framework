e3eb72ab4f4bf66b2a13f5332cbdd36d3de7ccf0
Juan Rodriguez <zenilt@gmail.com>
2014-04-21 18:11:43 -0300

    framework avances

--- web/app/ScriptApplication.php
+++ web/app/ScriptApplication.php
@@ -45,34 +45,37 @@ class ScriptApplication extends Application {
         $data = new PostgreSQLData($this->config);
         $query = "SELECT table_name FROM information_schema.tables WHERE table_schema='public'";
         $result = $data->performRead($query);
+        $classes = array();
         while ($row = $data->readNext($result)) {
-            $string = "";
-            $class = $cammelName = strtoupper(substr($row['table_name'], 0, 1)) . substr($row['table_name'], 1);
-            $string .= "<?php\n";
-            $string .= "class $class extends DataObject {\n";
-            $string .= "\n";
-            $string .= "}\n";
-            $string .= "?>\n";
-            $string .= "\n";
-            $string .= "\n";
-            $string .= "\n";
-            $string .= "\n";
-            $string .= "\n";
-            $string .= "\n";
-            $string .= "function $setter(\$value) { \n";
-            $string .= "function $setter(\$value) { \n";
-            $string .= "    \$this->$item = \$value; \n";
-            $string .= "} \n";
-            $string .= " \n";
-            $string .= "function $getter() { \n";
-            $string .= "    return \$this->$item; \n";
-            $string .= "} \n";
-            $string .= " \n";
+            $class = $row['table_name'];
+            $classes [] = $class;
+        }
+
+        foreach ($classes as $class) {
+            $query = "SELECT column_name FROM information_schema.columns WHERE table_name ='$class'";
+            $result = $data->performRead($query);
+            echo "begin class $class \n";
+            while ($row = $data->readNext($result)) {
+                $column = $row['column_name'];
+                echo 'private $' . $column . ";\n";
+            }
+            echo "end class $class \n";
+        }
+        
+        foreach ($classes as $class) {
+            $query = "SELECT column_name FROM information_schema.columns WHERE table_name ='$class'";
+            $result = $data->performRead($query);
+            echo "begin class $class \n";
+            while ($row = $data->readNext($result)) {
+                $column = $row['column_name'];
+                echo "\"$column\" => [\"name\" => \"$column\", \"type\" => PropertyTypes::\$_LONG , \"required\" => true, \"serializable\" => true, \"updatenull\" => true, \"hidden\" => false, \"private\" => false],\n";
+            }
+            echo "end class $class \n";
         }
     }
 
     private function generatePassword() {
-        $clave = 'admin';
+        $clave = $this->navigation = $this->getUrlParam("password", "string");
         echo $clave . " converted to " . $this->blowfishCrypt($clave, 10);
         echo "\n";
     }
